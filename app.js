763040f078373c75390a9d0c81c88201ecf498a5
Kaique Silva de Jesus <65189426+NayseBoy@users.noreply.github.com>
2023-06-12 15:43:09 -0300

    reorganizando o app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 
-function escreveNome (idade) {
-    return document.write("Kaique " + idade);
-}
-
 const idade = 18;
 
-console.log(escreveNome(idade));
+function escreveNome (idade) {
+    console.log("Kaique " + idade);
+}
+
+escreveNome(idade);
